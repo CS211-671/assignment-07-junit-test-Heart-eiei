f3fa4de7f3d35e0d321fe886ccd5305ae427e824
Supawit <supawit.kr@ku.th>
2024-08-22 16:47:42 +0700

    test Student and StudentList

--- src/main/java/ku/cs/models/Student.java
+++ src/main/java/ku/cs/models/Student.java
@@ -31,6 +31,19 @@ public class Student {
 
     public String grade()
     {
+        if (score >= 80 ) {
+            return "A";
+        } else if (score >= 70 ) {
+            return "B";
+        } else if (score >= 60 ) {
+            return "C";
+        } else if (score >= 50 ) {
+            return "D";
+        } else if (score >= 40 ) {
+            return "F";
+        }
+
+
         return "F";
     }
 
